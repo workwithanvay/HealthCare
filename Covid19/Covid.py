b8e768560c75cf160c932eda5fb745981c6d9e6d
Anvay Nikam <59839949+workwithanvay@users.noreply.github.com>
2020-05-09 18:45:20 +0530

    Add files via upload

---
+++ Covid19/Covid.py
@@ -0,0 +1,106 @@
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Dropout
+import pickle
+import matplotlib as plt
+
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a third convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dropout(p = 0.1))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('train',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('test',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch =180,
+                         epochs = 6,
+                         validation_data = test_set,
+                         validation_steps = 48)
+
+
+# Part 3 - Making new predictions
+'''
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('val/sample.jpeg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'Normal'
+else:
+    prediction = 'Pneumonia'
+'''
+
+
+
+
+# save the model to disk
+filename = 'final_corona_model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+
+    
+# load the model from disk
+loaded_model = pickle.load(open(filename, 'rb'))
+
+#Making predictions using saved model
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('val/sample.jpeg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = loaded_model.predict(test_image)
+if result[0][0] == 1:
+    prediction = 'Corona'
+else:
+    prediction = 'Normal'
+print(prediction)
+
+#is Normal
